fd9783d093247d17a73781c1523fe09a4063fc62
hoThanhThien <154042780+hoThanhThien@users.noreply.github.com>
2024-06-18 21:33:13 +0700

    Add files via upload

---
+++ baiTap2qlSach_083205012941/baiTap2qlSach_083205012941/Book.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+class Book
+{
+private:
+	/*Tên sách(chuỗi)
+		•	Tác giả(chuỗi, tối đa 5 tác giả)
+		•	Nhà xuất bản(chuỗi)
+		•	Năm xuất bản(số nguyên)*/
+	string tenSach;
+	vector<string> tacGia;
+	string nhaXuatBan;
+	int NamXuatBan;
+public:
+	
+
+	Book(const string& tenSach, const vector<string>& tacGia, const string& nhaXuatBan, int NamXuatBan);
+		
+	string gettenSach();
+	vector<string> gettacGia();
+	string getnhaXuatBan();
+	int getNamXuatBan();
+
+
+	void settenSach(string tenSach);
+	void settacGia(vector<string> tacGia);
+	void setnhaXuatBan(string nhaXuatBan);
+	void setNamXuatBan(int NamXuatBan);
+
+	
+};
+
---
+++ baiTap2qlSach_083205012941/baiTap2qlSach_083205012941/main.cpp
@@ -0,0 +1,81 @@
+#include"LinkedList.h"
+void menu()
+{
+	cout << "1.Them Sach"<<endl;
+	cout << "2.Dem sl" << endl;
+	cout << "3.tim Sach" << endl; 
+	cout << "4.Thoat" << endl;
+}
+
+int main() {
+	int luaChon;
+	LinkedList thuVien;
+	do {
+	
+		menu();
+		cout << "Nhap lua chon cua ban :\n";
+		cin >> luaChon;
+		cin.ignore();
+
+		switch (luaChon)
+
+		{
+		case 1: {
+			int slTacGia, NSX;
+			vector<string> TacGia;
+			string NXB, tenSach ;
+			cout << "Nhap Ten sach cua ban :\n";
+			getline(cin, tenSach);
+			do
+			{
+				cout << "Nhap sl tac gia cua ban(<=5) :\n";
+				cin >> slTacGia;
+				cin.ignore();
+
+			} while (slTacGia>5 || slTacGia < 1);
+			for (int i = 0; i < slTacGia; i++)
+			{
+				string tenTacGia;
+				cout << "Nhap tac gia thu " << i + 1 << ": ";
+				getline(cin, tenTacGia);
+				TacGia.push_back(tenTacGia);
+
+			}
+			cout << "Nhap nha xuat ban: ";
+			getline(cin, NXB);
+			cout << "Nhap nam xuat ban: ";
+			cin >> NSX;
+			cin.ignore();
+			Book book(tenSach, TacGia, NXB, NSX);
+			thuVien.themDau(book);
+			thuVien.hienThiDanhSach();
+			break;
+		}
+		case 2:
+		{
+			string tenTacGia;
+			cout << "Nhap ten tac gia: ";
+			getline(cin, tenTacGia);
+			int sl = thuVien.demSachCuaTG(tenTacGia);
+			cout << "So luong sach cua tac gia la : " << sl;
+			break;
+		}
+		case 3:
+		{
+			string NXB;
+			int NSX;
+			cout << " Nhap NXB: \n";
+			getline(cin, NXB);
+			cout << " Nhap NSX: \n";
+			cin >> NSX;
+			cin.ignore();
+			thuVien.timSach(NXB, NSX);
+			break;
+		}
+		default:
+			cout << "lua chon ko hop le\n";
+			break;
+		}
+	} while (luaChon!=4);
+	
+}
